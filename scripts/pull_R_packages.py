16d497ee37e0a51b9acc14c3981809e73e0a89c7
Rhythm-bbr <rhythm.bbr@gmail.com>
2025-06-28 18:43:59 +0530

    Update pull_R_packages.py

--- scripts/pull_R_packages.py
+++ scripts/pull_R_packages.py
@@ -10,6 +10,7 @@ from pyquery import PyQuery as pq
 import urllib
 import codecs
 import random
+import numpy as np
 
 text_file = codecs.open("Packages.txt", encoding='utf-8', mode="w")
 d = pq(url='http://cran.r-project.org/web/views/MachineLearning.html',
